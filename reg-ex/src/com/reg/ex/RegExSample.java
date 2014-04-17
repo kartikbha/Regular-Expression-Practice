98d08243753a305debd89f8af39e9047e9224e5f
Kartikbha <bhatnagar.kartik@gmail.com>
2014-04-17 22:31:38 +0530

    removed space

--- reg-ex/src/com/reg/ex/RegExSample.java
+++ reg-ex/src/com/reg/ex/RegExSample.java
@@ -3,11 +3,7 @@ package com.reg.ex;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
-
 public class RegExSample {
-	
-	
 
 	/**
 	 * use regular expression to extract quotation text out of question.
@@ -18,10 +14,11 @@ public class RegExSample {
 	public static String extractQuotationText(String quotation) {
 		// question format 1 -> how much is pish tegj glob glob ?
 		// question format 2 -> how many Credits is glob prok silver ?
-		final Pattern questionFormat1 = Pattern.compile("(how much is|how many Credits is)(.+?)\\?");
-		String questionValue = getQuestionValue(questionFormat1,quotation,new Integer[]{0,1,2},2);
-		
-		return questionValue;
+		final Pattern questionFormat1 = Pattern
+				.compile("(how much is|how many Credits is)(.+?)\\?");
+		String questionValue = getQuestionValue(questionFormat1, quotation,
+				new Integer[] { 0, 1, 2 }, 2);
+    	return questionValue;
 	}
 
 	/**
@@ -31,22 +28,17 @@ public class RegExSample {
 	 * @param str
 	 * @return
 	 */
-	private static String getQuestionValue(final Pattern ptn, final String str,Integer[] fetch,Integer grp) {
+	private static String getQuestionValue(final Pattern ptn, final String str,
+			Integer[] fetch, Integer grp) {
 		String questionValue = null;
 		final Matcher matcher = ptn.matcher(str);
 		if (matcher.find()) {
 			questionValue = matcher.group(grp);
-			for(int index : fetch){
-				System.out.println(" matcher.group() "+index+" "+matcher.group(index));
+			for (int index : fetch) {
+				System.out.println(" matcher.group() " + index + " "
+						+ matcher.group(index));
 			}
-			
-		}
-		
-		
+    	}
 		return questionValue;
-
-	}
+     }
 }
-
-
-
