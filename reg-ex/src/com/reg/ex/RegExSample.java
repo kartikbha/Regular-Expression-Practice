584ef89b612fc40b233ea19c3b3da690268626b3
Kartikbha <bhatnagar.kartik@gmail.com>
2014-04-20 14:28:02 +0530

    more addition of reg ex

--- reg-ex/src/com/reg/ex/RegExSample.java
+++ reg-ex/src/com/reg/ex/RegExSample.java
@@ -1,16 +1,15 @@
 package com.reg.ex;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExSample {
 
-	/**
-	 * use regular expression to extract quotation text out of question.
-	 * 
-	 * @param quotation
-	 * @return
-	 */
 	public static String extractQuotationText(String quotation) {
 		// question format 1 -> how much is pish tegj glob glob ?
 		// question format 2 -> how many Credits is glob prok silver ?
@@ -18,27 +17,54 @@ public class RegExSample {
 				.compile("(how much is|how many Credits is)(.+?)\\?");
 		String questionValue = getQuestionValue(questionFormat1, quotation,
 				new Integer[] { 0, 1, 2 }, 2);
-    	return questionValue;
+		return questionValue;
 	}
 
-	/**
-	 * fetch and return matched expression
-	 * 
-	 * @param ptn
-	 * @param str
-	 * @return
-	 */
+	
+	public static String regExPatternExecutor(String text,String pattern) {
+		final Pattern format = Pattern
+				.compile(pattern);
+		String questionValue = getQuestionValue(format, text,
+				new Integer[] {0}, 0);
+		return questionValue;
+	}
+	
 	private static String getQuestionValue(final Pattern ptn, final String str,
 			Integer[] fetch, Integer grp) {
 		String questionValue = null;
 		final Matcher matcher = ptn.matcher(str);
-		if (matcher.find()) {
+		while (matcher.find()) {
 			questionValue = matcher.group(grp);
+			System.out.println(" matcher.start(grp)  "
+					+ matcher.start(grp));
+			System.out.println(" matcher.end(grp)  "
+					+ matcher.end(grp));
+			matcher.end(grp);
 			for (int index : fetch) {
 				System.out.println(" matcher.group() " + index + " "
 						+ matcher.group(index));
 			}
-    	}
+		}
 		return questionValue;
-     }
+	}
+
+	public static String readFile(final String filePath) {
+		final StringBuffer buffer = new StringBuffer();
+		try {
+			final FileInputStream fis = new FileInputStream(filePath);
+			final InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+			final Reader reader = new BufferedReader(isr);
+			int character;
+			while ((character = reader.read()) > -1) {
+				buffer.append((char) character);
+			}
+			reader.close();
+			isr.close();
+			fis.close();
+
+		} catch (final IOException e) {
+			System.out.println(" e " + e);
+		}
+		return buffer.toString();
+	}
 }
--- reg-ex/src/com/reg/ex/test/RegExSampleTest.java
+++ reg-ex/src/com/reg/ex/test/RegExSampleTest.java
@@ -20,4 +20,44 @@ public class RegExSampleTest {
 		Assert.assertEquals("glob prok silver",questionText.trim());
 	}
 	
+	@Test
+	public void greedyIdentifierTest(){
+		
+		String inputTexts = RegExSample.readFile("C:/Users/Kartik/reg-ex/Regular-Expression-Practice/reg-ex/src/13chil.txt");
+		RegExSample.regExPatternExecutor(inputTexts,".*\\bgood\\b.*?");
+	
+	}
+	
+	
+	@Test
+	public void findDateFormatMM_DD_YY(){
+		
+		String inputTexts = RegExSample.readFile("C:/Users/Kartik/reg-ex/Regular-Expression-Practice/reg-ex/src/dateFormats.txt");
+		// not valid as it also match - 12345678.   3 & 6 on place holder .
+		//RegExSample.regExPatternExecutor(inputTexts,"\\d\\d.\\d\\d.\\d\\d");
+		
+		// matches 23/34/23
+		//RegExSample.regExPatternExecutor(inputTexts,"\\d\\d[-/]\\d\\d[-/]\\d\\d");
+	}
+	
+	@Test
+	public void IpAddressText(){
+		
+		String inputTexts = RegExSample.readFile("C:/Users/Kartik/reg-ex/Regular-Expression-Practice/reg-ex/src/IpAddress.txt");
+		// ip address format - 255.x.x.x - forward +ve look ahead. find ips where ip does have 255 as start.
+		// RegExSample.regExPatternExecutor(inputTexts,"(?=255)(\\d*).(\\d*).(\\d*).(\\d*)");
+		// better way is to use  [.] instead.
+		// ip address format - 255.x.x.x - forward -ve look ahead - fine ips where ip doesn't have 255 as start.
+		RegExSample.regExPatternExecutor(inputTexts,"(?!255)(\\d*)[.](\\d*)[.](\\d*)[.](\\d*)");
+		// ip address format - 255.x.x.x - forward -ve look ahead - fine ips where ip doesn't have 255 as start.
+		//RegExSample.regExPatternExecutor(inputTexts,"(\\d*)[.](?<=255)[.](\\d*)[.](\\d*)");
+		
+	}
+	
+	@Test
+	public void readTest(){
+		String readText = RegExSample.readFile("C:/Users/Kartik/reg-ex/Regular-Expression-Practice/reg-ex/src/13chil.txt");
+		System.out.println("read text "+ readText);
+	}
+	
 }
